efcd54d70eacc6e110a95706d82c1d2eda46ea88
jennie snider <jenniesnider@jennies-MacBook-Pro.local>
2017-08-09 20:44:51 -0700

    adding routes

--- server.js
+++ server.js
@@ -30,3 +30,98 @@ db.on("error", function(error) {
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
+
+
+//*********************************ROUTE TIME!!!!************************************************************
+
+//make the root redirect to the scrape route so users don't have to click a button to load news
+app.get("/", function(req, res){
+	res.redirect("/scrape");
+});
+
+// Scrape Clickhole
+app.get("/scrape", function(req, res) {
+
+	request("http://www.clickhole.com/", function(error, response, html){
+		var $ = cheerio.load(html);
+		$("article").each(function(i, element){
+			var result = {};
+			result.title = $(this).children("a").text();
+      		result.link = $(this).children("a").attr("href");
+      		var entry = new Article(result);
+      		entry.save(function(err, doc){
+      			if(err){
+      				console.log(error);
+      			}else{
+      				console.log(doc);
+      			}
+      		});
+
+		});
+	});
+		res.send("Scrape Complete");
+});
+
+//get scraped articles
+app.get("/articles", function(req, res) {
+  Article.find({}, function(error, doc) {
+    if (error) {
+      console.log(error);
+    }else {
+      res.json(doc);
+    }
+  });
+});
+
+//grab an acticle by id so we can do stuff to it
+app.get("/articles/:id", function(req, res) {
+  Article.findOne({ "_id": req.params.id })
+  .populate("note")
+  .exec(function(error, doc) {
+    if (error) {
+      console.log(error);
+    }else {
+    	res.json(doc);
+    }
+  });
+});
+
+//make or edit notes
+app.post("/articles/:id", function(req, res) {
+  var newNote = new Note(req.body);
+  newNote.save(function(error, doc) {
+    if (error) {
+      console.log(error);
+    }else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+      .exec(function(err, doc) {
+        if (err) {
+          console.log(err);
+        }else {
+          res.send(doc);
+        }
+      });
+    }
+  });
+});
+
+
+
+//listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
